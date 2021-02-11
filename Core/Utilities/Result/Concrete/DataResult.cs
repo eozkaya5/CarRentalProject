c7d921a8045ce239f8947c0d7f472bb0b2a62c84
eozkaya5 <eozkaya675@gmail.com>
2021-02-12 00:37:01 +0300

    Result yapısı, veri kullanarak oluşturuldu. ve IResult implement edildi.

---
+++ Core/Utilities/Result/Concrete/DataResult.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Result.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Result.Concrete
+{
+    public class DataResult<T> : Result, IDataResult<T>
+    {
+        public DataResult(T data, bool success, string message) : base(success)
+        {
+            Data = data;
+        }
+        public DataResult(T data, bool deliver) : base(deliver)
+        {
+            Data = data;
+        }
+        public T Data { get; }
+    }
+}
