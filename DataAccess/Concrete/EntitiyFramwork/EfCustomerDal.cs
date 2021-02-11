c4ba6fc9e7fd63e6dd13d8fd6c05ad1caea52898
eozkaya5 <eozkaya675@gmail.com>
2021-02-12 00:42:01 +0300

    Oluşturulan dal sınıfına EfEntityRepositoryBase ve ICustomerDal implement edildi.

---
+++ DataAccess/Concrete/EntitiyFramwork/EfCustomerDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntitiyFramwork
+{
+    public class EfCustomerDal : EfEntityRepositoryBase<Customer, CarRentalContext>, ICustomerDal
+    {
+        public void GetDetail()
+        {
+        }
+    }
+}
