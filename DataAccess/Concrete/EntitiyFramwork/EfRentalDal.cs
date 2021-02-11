5914ce06fe5ca25939728b1cb9d004ccc63265d5
eozkaya5 <eozkaya675@gmail.com>
2021-02-12 00:45:10 +0300

    Oluşturulan sınıfa EfEntityRepositoryBase ve IRentalDAl implemnt edildi.

---
+++ DataAccess/Concrete/EntitiyFramwork/EfRentalDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntitiyFramwork
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, CarRentalContext>, IRentalDal
+    {
+        public void GetDetail()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
