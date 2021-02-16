af1dfc843e2248ab34b8292e4ca1c2a2bef2ffb6
eozkaya5 <eozkaya675@gmail.com>
2021-02-16 15:54:40 +0300

    Attribute kullanıldı.

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -2,15 +2,18 @@
 using Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Entities.Concrete
 {
+    [Table("Cars")]// Car tablosu sql' de Cars tablosuna denk gelir
     public class Car : IEntity
     {
         public int Id { get; set; }
         public int BrandId { get; set; }
         public int ColorId { get; set; }
+        [RequiredProperty]//Carname girmek zorunda
         public string CarName { get; set; }
         public int Modelyear { get; set; }
         public decimal DailyPrice { get; set; }
@@ -18,3 +21,8 @@ namespace Entities.Concrete
 
     }
 }
+//Attribute: Öz nitelikler
+//Çalışma anında nesnelerin özelliklerine methodlara anlam katmak için kullanırız
+//İş kuralları koyabilirsiniz.
+
+//[Obsolete]// Bu methodu kullanma
