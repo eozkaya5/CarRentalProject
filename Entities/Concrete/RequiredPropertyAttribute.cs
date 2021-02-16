84f9fda701cca3d60566150b8239c368eafd8fc3
eozkaya5 <eozkaya675@gmail.com>
2021-02-16 16:15:50 +0300

    "Attrıbute nerlerde nasıl kullanılabilir ?" Sorusuna cevap verildi.

--- Entities/Concrete/RequiredPropertyAttribute.cs
+++ Entities/Concrete/RequiredPropertyAttribute.cs
@@ -2,6 +2,12 @@
 
 namespace Entities.Concrete
 {
+    //[AttributeUsage(AttributeTargets.Class)]// Sadece class'larda kullanabilrsin
+    // nerede kullanabileceğimizi belirleyebiliriz
+    //[AttributeUsage(AttributeTargets.Class ,AllowMultiple =true)]// Birden fazla kullanbilir miyiz
+
+
+    [AttributeUsage(AttributeTargets.All)]// her yerde kullanabilirsin
     internal class RequiredPropertyAttribute : Attribute
     {
         // Attribute  oluşturuldu
