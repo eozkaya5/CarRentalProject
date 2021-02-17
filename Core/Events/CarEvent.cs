a759c8152c8cdf3ac345e26d710a5a354bac5dd3
eozkaya5 <eozkaya675@gmail.com>
2021-02-17 15:49:27 +0300

    Event için oluşturulan sınıfa işlem eklendi.

---
+++ Core/Events/CarEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Events
+{
+    // Uygulamalarda bir haraket olduğunda bu harakete ek olarak işlem yapmak isterken kullanılır
+    public delegate void StockControl();
+    public class CarEvent
+    {
+        private int _stock;
+
+        public CarEvent(int stock)
+        {
+            _stock = stock;
+        }
+
+        public event StockControl StockControlEvent;
+        public string CarName { get; set; }
+        public int Stock
+        {
+            get { return _stock; }
+            set
+            {
+                _stock = value;
+                if (value <= 15 && StockControlEvent != null)
+                {
+                    StockControlEvent();
+                }
+            }
+        }
+
+        public void Sell(int amout)
+        {
+            Stock -= amout;
+            Console.WriteLine("Stock amout : {0}", Stock);
+        }
+    }
+}
