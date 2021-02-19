277b7b7aa3d4cf651ab6a11e94700ae8888526e1
eozkaya5 <eozkaya675@gmail.com>
2021-02-19 23:46:57 +0300

    save.

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -9,7 +9,7 @@ using System.Text;
 
 namespace Business.DependencyResolvers.Autofac
 {
-    public class AutofacBusinessModule: Module
+    public class AutofacBusinessModule: Module 
     {
         protected override void Load(ContainerBuilder builder)
         {
