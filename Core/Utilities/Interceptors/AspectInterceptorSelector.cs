21b64b4d97c39627ca21955fffd1627d9a3ffce6
eozkaya5 <eozkaya675@gmail.com>
2021-02-21 02:27:16 +0300

    Yorum satırı eklendi.

--- Core/Utilities/Interceptors/AspectInterceptorSelector.cs
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -13,12 +13,12 @@ namespace Core.Utilities.Interceptors
         public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] ınterceptors)
         {
             var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>(true).ToList();
-
+            //sınıfları oku
             var methodAttributes = type.GetMethod(method.Name).GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
-
+            //methodları oku
             classAttributes.AddRange(methodAttributes);
 
-            return classAttributes.OrderBy(x => x.Priority).ToArray();
+            return classAttributes.OrderBy(x => x.Priority).ToArray();//öncelik değerine göre sırala
         }
     }
 }
