5a17efb7680081ec57e4c6955aa53f6a429078b3
eozkaya5 <eozkaya675@gmail.com>
2021-02-21 02:15:36 +0300

    Methodların yerine kullanılacak "Intercept " methodu oluşturuldu.

---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple =true, Inherited =true)]
+    public abstract class MethodInterceptionBaseAttribute: Attribute,IInterceptor
+    {
+        public int Priority { get; set; }//Öncelik anlamına gelir
+   
+        public void Intercept(IInvocation invocation)
+        {
+            
+        }
+    }
+}
