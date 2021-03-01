5496561cccc64862d0033ddb4ac0898e2fc3f8c9
eozkaya5 <eozkaya675@gmail.com>
2021-03-02 01:09:38 +0300

    düzenlendi.

--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -8,11 +8,9 @@ namespace Entities.Concrete
     public class Customer : IEntity
     {
         public int Id { get; set; }
-
         public int UserId { get; set; }
-
         public string CompanyName { get; set; }
 
-        public User User { get; set; }
+      
     }
 }
