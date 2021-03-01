484090b587828759da0d2662e6a9486181691c11
eozkaya5 <eozkaya675@gmail.com>
2021-03-02 00:38:01 +0300

    Entity framework için dal oluşturuldu.

--- DataAccess/Concrete/EntitiyFramwork/EfUserDal.cs
+++ DataAccess/Concrete/EntitiyFramwork/EfUserDal.cs
@@ -1,4 +1,5 @@
 ﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
@@ -7,7 +8,11 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntitiyFramwork
 {
-    public class EfUserDal:EfEntityRepositoryBase<User, CarRentalContext>,IUserDal
+    public class EfUserDal : EfEntityRepositoryBase<User, CarRentalContext>, IUserDal
     {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
