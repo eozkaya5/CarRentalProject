b3d594a03d028d140904efe50466890118f02acb
eozkaya5 <eozkaya675@gmail.com>
2021-03-02 00:27:14 +0300

    Değerler düzeltildi.Özellik eklendi.

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -8,16 +8,12 @@ namespace Entities.Concrete
     public class User:IEntity
     {
         public int Id { get; set; }
-
         public string FirstName { get; set; }
-
         public string Lastname { get; set; }
+        public byte[] Email { get; set; }
+        public byte[] Password { get; set; }
+        public bool Status { get; set; }
 
-        public string Email { get; set; }
-
-        public string Password { get; set; }
-
-        public List<Customer> Customers { get; set; }
 
     }
 }
