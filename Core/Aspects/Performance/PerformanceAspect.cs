e902bdf7c7da8f5690fedc397379d08a540baefe
eozkaya5 <eozkaya675@gmail.com>
2021-03-04 19:33:39 +0300

    Performansı kontrol altına alabilmek için operasyon yazıldı.

---
+++ Core/Aspects/Performance/PerformanceAspect.cs
@@ -0,0 +1,36 @@
+﻿using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Aspects.Performance
+{
+    public class PerformanceAspect : MethodInterception
+    {
+        private int _interval;
+        private Stopwatch _stopwatch; //Ne kadar sürecek?
+
+        public PerformanceAspect(int interval)
+        {
+            _interval = interval;
+            _stopwatch = ServiceTool.ServiceProvider.GetService<Stopwatch>();
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            _stopwatch.Start();// Kronometre başlattı.
+        }
+        protected override void OnAfter(IInvocation invocation)
+        {
+            if (_stopwatch.Elapsed.TotalSeconds > _interval)
+            {
+                Debug.WriteLine($"Performance : {invocation.Method.DeclaringType.FullName}.{invocation.Method.Name}-->{_stopwatch.Elapsed.TotalSeconds}");
+            }
+            _stopwatch.Reset();
+        }
+    }
+}
